dadde21e143576fe2827c926c43d075751856ad7
Levent Kaya <leventkayadev@gmail.com>
2026-08-14 23:55:07 +0300

    [fmt] format source code

--- test/source/harness.h
+++ test/source/harness.h
@@ -7,42 +7,40 @@
 
 static int weft_test_failures = 0;
 
-#define TEST_CHECK(cond)                                                     \
-    do {                                                                     \
-        if (!(cond)) {                                                       \
-            fprintf(stderr, "FAIL %s:%d: %s\n", __FILE__, __LINE__, #cond);  \
-            weft_test_failures++;                                            \
-        }                                                                    \
+#define TEST_CHECK(cond)                                                    \
+    do {                                                                    \
+        if (!(cond)) {                                                      \
+            fprintf(stderr, "FAIL %s:%d: %s\n", __FILE__, __LINE__, #cond); \
+            weft_test_failures++;                                           \
+        }                                                                   \
     } while (0)
 
-#define TEST_EQ_INT(a, b)                                                    \
-    do {                                                                     \
-        long long va_ = (long long)(a);                                      \
-        long long vb_ = (long long)(b);                                      \
-        if (va_ != vb_) {                                                    \
-            fprintf(stderr, "FAIL %s:%d: %s == %s (%lld != %lld)\n",         \
-                    __FILE__, __LINE__, #a, #b, va_, vb_);                   \
-            weft_test_failures++;                                            \
-        }                                                                    \
+#define TEST_EQ_INT(a, b)                                                                        \
+    do {                                                                                         \
+        long long va_ = (long long)(a);                                                          \
+        long long vb_ = (long long)(b);                                                          \
+        if (va_ != vb_) {                                                                        \
+            fprintf(stderr, "FAIL %s:%d: %s == %s (%lld != %lld)\n", __FILE__, __LINE__, #a, #b, \
+                    va_, vb_);                                                                   \
+            weft_test_failures++;                                                                \
+        }                                                                                        \
     } while (0)
 
-#define TEST_EQ_STR(a, b)                                                    \
-    do {                                                                     \
-        const char *sa_ = (a);                                               \
-        const char *sb_ = (b);                                               \
-        if (strcmp(sa_, sb_) != 0) {                                         \
-            fprintf(stderr, "FAIL %s:%d: \"%s\" != \"%s\"\n",                \
-                    __FILE__, __LINE__, sa_, sb_);                           \
-            weft_test_failures++;                                            \
-        }                                                                    \
+#define TEST_EQ_STR(a, b)                                                                    \
+    do {                                                                                     \
+        const char *sa_ = (a);                                                               \
+        const char *sb_ = (b);                                                               \
+        if (strcmp(sa_, sb_) != 0) {                                                         \
+            fprintf(stderr, "FAIL %s:%d: \"%s\" != \"%s\"\n", __FILE__, __LINE__, sa_, sb_); \
+            weft_test_failures++;                                                            \
+        }                                                                                    \
     } while (0)
 
-#define TEST_RUN(fn)                                                         \
-    do {                                                                     \
-        int before_ = weft_test_failures;                                    \
-        fn();                                                                \
-        fprintf(stderr, "%s %s\n",                                           \
-                weft_test_failures == before_ ? "ok  " : "FAIL", #fn);       \
+#define TEST_RUN(fn)                                                                      \
+    do {                                                                                  \
+        int before_ = weft_test_failures;                                                 \
+        fn();                                                                             \
+        fprintf(stderr, "%s %s\n", weft_test_failures == before_ ? "ok  " : "FAIL", #fn); \
     } while (0)
 
 #define TEST_MAIN_END() return weft_test_failures == 0 ? 0 : 1
--- test/source/weft_test.c
+++ test/source/weft_test.c
@@ -1,11 +1,11 @@
-#include "weft/weft.h"
-
 #include <string.h>
 
-int main(int argc, char const* argv[])
-{
-  (void)argc;
-  (void)argv;
+#include "weft/weft.h"
 
-  return strcmp(exported_function(), "weft") == 0 ? 0 : 1;
+int main(int argc, const char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    return strcmp(exported_function(), "weft") == 0 ? 0 : 1;
 }
